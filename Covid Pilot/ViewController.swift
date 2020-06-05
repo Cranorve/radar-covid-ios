4bbd871e1e88067e3ed4f8de10f8af1928df0407
Lino <masterlino@gmail.com>
2020-06-05 15:07:44 +0200

    First Project Draft

---
+++ Covid Pilot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid Pilot
+//
+//  Created by Lino Bustamante on 04/06/2020.
+//  Copyright © 2020 Indra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
