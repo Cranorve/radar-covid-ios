a79c185630529e9fcfc1441a982d91b17626d576
alopezh <alopezh@indra.es>
2020-06-09 08:04:43 +0200

    hide navigation bar

--- Covid Pilot/Application/SceneDelegate.swift
+++ Covid Pilot/Application/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let navigationController = UINavigationController()
+        navigationController.setNavigationBarHidden(true, animated: false)
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
