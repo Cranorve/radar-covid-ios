b0a2d19aeb450716694bcbf5b7bcc2de04f3a505
Lino <masterlino@gmail.com>
2020-06-10 18:18:38 +0200

    Myhealth reported storyboard

---
+++ Covid Pilot/Modules/MyHealth/MyHealthReportedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MyHealthReportedViewController.swift
+//  Covid Pilot
+//
+//  Created by Lino Bustamante on 10/06/2020.
+//  Copyright © 2020 Indra. All rights reserved.
+//
+
+import UIKit
+
+class MyHealthReportedViewController: UIViewController {
+
+    @IBAction func onBack(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
