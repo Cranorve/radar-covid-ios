57e97326c10922f8fe2c7ef7ad3f429e5744a762
Lino <masterlino@gmail.com>
2020-06-15 15:31:42 +0200

    Accept terms checkbox logic implemented for the onboarding storyboard

--- Covid Pilot/Modules/OnBoarding/OnBoardingViewController.swift
+++ Covid Pilot/Modules/OnBoarding/OnBoardingViewController.swift
@@ -12,22 +12,56 @@ class OnBoardingViewController: UIViewController {
     
     var onBoardingCompletedUseCase: OnboardingCompletedUseCase?
     var router: AppRouter?
+    
+    private var termsAccepted: Bool = false
 
+    @IBOutlet weak var checkBoxImage: UIImageView!
+   
+    @IBOutlet weak var acceptTermsLabel: UILabel!
+    
+    @IBOutlet weak var reminderAcceptTermsView: UIView!
+    
+    
     @IBAction func onOk(_ sender: Any) {
-        router?.route(to: Routes.Terms, from:self)
+        if (!termsAccepted){
+            //show reminder view
+            reminderAcceptTermsView.isHidden = false
+        }
+        else {
+            reminderAcceptTermsView.isHidden = false
+            router?.route(to: Routes.Terms, from:self)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidLoad()
-//        if (onBoardingCompletedUseCase?.isOnBoardingCompleted() ?? false) {
-//            router?.route(to: Routes.Home, from: self)
-//        }
-       router?.route(to: Routes.Home, from: self)
+        //if (onBoardingCompletedUseCase?.isOnBoardingCompleted() ?? false) {
+        //    router?.route(to: Routes.Home, from: self)
+        //}
+        router?.route(to: Routes.Home, from: self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Adding tapgesture to the Accept terms checkboxImage and label
+        acceptTermsLabel.isUserInteractionEnabled = true
+        checkBoxImage.isUserInteractionEnabled = true
+        acceptTermsLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(userDidTapLabel(tapGestureRecognizer:))))
+        checkBoxImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(userDidTapLabel(tapGestureRecognizer:))))
 
+    }
+    
+    
+    @objc func userDidTapLabel(tapGestureRecognizer: UITapGestureRecognizer) {
+        if (!termsAccepted) {
+            checkBoxImage.image = UIImage(named:"CheckboxSelected")
+            termsAccepted = true
+        }
+        else {
+            checkBoxImage.image = UIImage(named:"CheckboxUnselected")
+            termsAccepted = false
+        }
     }
 
 }
