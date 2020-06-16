46ad3f143919568abc51cb62e919394a8221af9c
Lino <masterlino@gmail.com>
2020-06-16 17:31:44 +0200

    exposition use case

--- Covid Pilot/Domain/ExpositionUseCase.swift
+++ Covid Pilot/Domain/ExpositionUseCase.swift
@@ -6,13 +6,42 @@
 //  Copyright © 2020 Indra. All rights reserved.
 //
 
+import DP3TSDK
 import Foundation
 import RxSwift
 
-class ExpositionUseCase {
+class ExpositionUseCase: DP3TTracingDelegate {
+    
+    func DP3TTracingStateChanged(_ state: TracingState) {
+        // TODO Update our current expositionInfo 
+    }
+    
     
     func getExpositionInfo() -> Observable<ExpositionInfo> {
-        let exposition = ExpositionInfo(level: ExpositionInfo.Level.LOW)
+        var tStatus : TracingState?
+        DP3TTracing.status { result in
+            switch result {
+            case let .success(state):
+                tStatus = state
+            case .failure:
+                break
+            }
+        }
+        var exposition: ExpositionInfo = ExpositionInfo(level: ExpositionInfo.Level.LOW)
+        if let _ = tStatus {
+            switch tStatus!.infectionStatus {
+            case .healthy:
+                exposition = ExpositionInfo(level: ExpositionInfo.Level.LOW)
+                
+            case .infected:
+                exposition = ExpositionInfo(level: ExpositionInfo.Level.HIGH)
+            
+            default:
+                exposition = ExpositionInfo(level: ExpositionInfo.Level.LOW)
+                
+            }
+        }
+        
         return .just(exposition)
     }
     
