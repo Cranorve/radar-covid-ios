d52296edd0b9c1b329c5f380d0b2748510bfa8e1
alopezh <alopezh@indra.es>
2020-06-21 12:11:37 +0200

    Changes in OnBoarding navigation

--- Covid Pilot/Modules/OnBoarding/OnBoardingViewController.swift
+++ Covid Pilot/Modules/OnBoarding/OnBoardingViewController.swift
@@ -18,9 +18,9 @@ class OnBoardingViewController: UIViewController {
     @IBOutlet weak var checkBoxImage: UIImageView!
    
     @IBOutlet weak var acceptTermsLabel: UILabel!
+    @IBOutlet weak var privacyLabel: UILabel!
     
-    @IBOutlet weak var reminderAcceptTermsView: UIView!
-    
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     @IBAction func onOk(_ sender: Any) {
         if (!termsAccepted){
@@ -36,9 +36,10 @@ class OnBoardingViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidLoad()
         if (onBoardingCompletedUseCase?.isOnBoardingCompleted() ?? false) {
-            router?.route(to: Routes.Home, from: self)
+//            router?.route(to: Routes.Home, from: self)
         }
 //        router?.route(to: Routes.Home, from: self)
+        loadTexts()
     }
     
     override func viewDidLoad() {
@@ -46,14 +47,20 @@ class OnBoardingViewController: UIViewController {
         
         //Adding tapgesture to the Accept terms checkboxImage and label
         acceptTermsLabel.isUserInteractionEnabled = true
-        checkBoxImage.isUserInteractionEnabled = true
-        acceptTermsLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(userDidTapLabel(tapGestureRecognizer:))))
-        checkBoxImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(userDidTapLabel(tapGestureRecognizer:))))
+        acceptView.isUserInteractionEnabled = true
+        privacyLabel.isUserInteractionEnabled = true
+        
+        acceptView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(userDidTapAccept(tapGestureRecognizer:))))
+        
+        acceptTermsLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(userDidTapTerms(tapGestureRecognizer:))))
+        
+        privacyLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(userDidTapPrivacy(tapGestureRecognizer:))))
 
     }
     
+    @IBOutlet weak var acceptView: UIView!
     
-    @objc func userDidTapLabel(tapGestureRecognizer: UITapGestureRecognizer) {
+    @objc func userDidTapAccept(tapGestureRecognizer: UITapGestureRecognizer) {
         if (!termsAccepted) {
             checkBoxImage.image = UIImage(named:"CheckboxSelected")
             termsAccepted = true
@@ -62,6 +69,32 @@ class OnBoardingViewController: UIViewController {
             checkBoxImage.image = UIImage(named:"CheckboxUnselected")
             termsAccepted = false
         }
+    }
+    
+    @objc func userDidTapTerms(tapGestureRecognizer: UITapGestureRecognizer) {
+
+    }
+    
+    @objc func userDidTapPrivacy(tapGestureRecognizer: UITapGestureRecognizer) {
+
+    }
+    
+    private func loadTexts() {
+        let attributedString = NSMutableAttributedString(string: "Nuestros compromisos\n\nAnonimato \nLa aplicación funciona sin revelar tu identidad ni la de tu smartphone. \nNO se recoge tu nombre, email, geolocalización ni tu teléfono\n\nDiscreción \nLas alertas de exposición se envían sin indicar cuándo y dónde se produjo la exposición.\n\nTú decides\nElige en todo momento si quieres desactivar el servicio o dejar de utilizar la App", attributes: [
+          .font: UIFont(name: "Muli-Light", size: 20.0)!,
+          .foregroundColor: UIColor(white: 0.0, alpha: 1.0)
+        ])
+        attributedString.addAttributes([
+          .font: UIFont(name: "Muli-Bold", size: 22.0)!,
+          .foregroundColor: UIColor(red: 112.0 / 255.0, green: 80.0 / 255.0, blue: 156.0 / 255.0, alpha: 1.0)
+        ], range: NSRange(location: 0, length: 21))
+        attributedString.addAttribute(.font, value: UIFont(name: "Muli-ExtraBold", size: 20.0)!, range: NSRange(location: 21, length: 12))
+        attributedString.addAttribute(.font, value: UIFont(name: "Muli-ExtraBold", size: 20.0)!, range: NSRange(location: 106, length: 2))
+        attributedString.addAttribute(.font, value: UIFont(name: "Muli-ExtraBold", size: 20.0)!, range: NSRange(location: 169, length: 12))
+        attributedString.addAttribute(.font, value: UIFont(name: "Muli-ExtraBold", size: 20.0)!, range: NSRange(location: 271, length: 11))
+        
+        descriptionLabel.attributedText = attributedString
+        
     }
 
 }
