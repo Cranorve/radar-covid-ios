aad34a65ab28ed7da90ab51fe713eeded6c60740
noel <ndcg9105@gmail.com>
2020-06-26 18:36:23 -0400

    fixed issues 0027558 0027619 0027560 0027674 0027557

--- Covid Pilot/Modules/Poll/FinishPollViewController.swift
+++ Covid Pilot/Modules/Poll/FinishPollViewController.swift
@@ -14,11 +14,19 @@ class FinishPollViewController: UIViewController, MFMailComposeViewControllerDel
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var phoneView: BackgroundView!
     
+    @IBOutlet weak var telephoneAsistant: UILabel!
     @IBOutlet weak var timeTableLabel: UILabel!
     @IBOutlet weak var phoneNumberLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let attributedString = NSMutableAttributedString(string: "Contacta con nosotros si tu riesgo de exposición en la aplicación es alto o si tienes cualquier incidencia sobre la aplicación.", attributes: [
+          .font: UIFont(name: "Muli-Light", size: 16.0)!,
+          .foregroundColor: UIColor(white: 0.0, alpha: 1.0)
+        ])
+        attributedString.addAttribute(.font, value: UIFont(name: "Muli-SemiBold", size: 16.0)!, range: NSRange(location: 22, length: 51))
+        attributedString.addAttribute(.font, value: UIFont(name: "Muli-SemiBold", size: 16.0)!, range: NSRange(location: 96, length: 10))
+        telephoneAsistant.attributedText = attributedString
         phoneView.isUserInteractionEnabled = true
         
         phoneView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onCallTap(tapGestureRecognizer:))))
@@ -27,9 +35,9 @@ class FinishPollViewController: UIViewController, MFMailComposeViewControllerDel
         phoneNumberLabel.text = Config.contactNumber
         
         phoneView.image = UIImage(named: "WhiteCard")
-        
-        emailLabel.isUserInteractionEnabled = true
-        emailLabel.addGestureRecognizer(  UITapGestureRecognizer(target: self, action: #selector(onEmailTap(tapGestureRecognizer:)))  )
+//        
+//        emailLabel.isUserInteractionEnabled = true
+//        emailLabel.addGestureRecognizer(  UITapGestureRecognizer(target: self, action: #selector(onEmailTap(tapGestureRecognizer:)))  )
     }
 
     @objc func onCallTap(tapGestureRecognizer: UITapGestureRecognizer) {
