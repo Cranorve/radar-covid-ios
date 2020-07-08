5cdf8103cf8a5f60687a3ad42898d471b65c3185
alopezh <alopezh@indra.es>
2020-07-08 17:44:03 +0200

    Localization in Xib

---
+++ Covid Pilot/Components/Localizable.swift
@@ -0,0 +1,13 @@
+//
+//  Localizable.swift
+//  Covid Pilot
+//
+//  Created by alopezh on 08/07/2020.
+//  Copyright © 2020 Indra. All rights reserved.
+//
+
+import Foundation
+
+protocol Localizable {
+    var localized: String { get }
+}
---
+++ Covid Pilot/Components/LocalizableLabel.swift
@@ -0,0 +1,19 @@
+//
+//  LocalizableLabel.swift
+//  Covid Pilot
+//
+//  Created by alopezh on 08/07/2020.
+//  Copyright © 2020 Indra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel : XibLocalizable {
+    @IBInspectable var locKey: String? {
+        get { return nil }
+        set(key) {
+            text = ""
+        }
+    }
+}
