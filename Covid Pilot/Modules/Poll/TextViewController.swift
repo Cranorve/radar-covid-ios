d28bdb7e076d1c730cb248b17b79b9aae4ec3e7d
Lino <masterlino@gmail.com>
2020-07-09 13:04:02 +0200

    0027800

--- Covid Pilot/Modules/Poll/TextViewController.swift
+++ Covid Pilot/Modules/Poll/TextViewController.swift
@@ -36,6 +36,10 @@ class TextViewController: UIViewController, UITextViewDelegate, QuestionControll
             textView.text = nil
             textView.textColor = UIColor.black
         }
+        
+        //resize textEditor
+        let constraint = self.textView.constraints.first(where: {$0.identifier == "textViewHeigh"})
+        constraint?.constant = 100
     }
     
     func textViewDidEndEditing(_ textView: UITextView) {
@@ -43,6 +47,9 @@ class TextViewController: UIViewController, UITextViewDelegate, QuestionControll
             textView.text = "Describir..."
             textView.textColor = UIColor.lightGray
         }
+        //resize textEditor
+        let constraint = self.textView.constraints.first(where: {$0.identifier == "textViewHeigh"})
+        constraint?.constant = 250
     }
     
     func textViewDidChange(_ textView: UITextView) {
@@ -52,6 +59,12 @@ class TextViewController: UIViewController, UITextViewDelegate, QuestionControll
     func endEditingTextView(){
         textView.endEditing(true)
     }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        //500 chars restriction
+        return textView.text.count + (text.count - range.length) <= 500
+    }
+    
 
     
     
