b3bec53de2522ba2d0349852f622f1c83b5f9230
alopezh <alopezh@indra.es>
2020-07-30 18:02:23 +0200

    Added web parameter

--- Covid Pilot/Domain/Model/CaData.swift
+++ Covid Pilot/Domain/Model/CaData.swift
@@ -12,16 +12,19 @@ struct CaData: Codable {
     
     public var id: String?
     public var description: String?
+    
     public var phone: String?
     public var email: String?
+    public var web: String?
     public var additionalInfo: String?
 
-    public init(id: String?, description: String?, phone: String?, email: String?, additionalInfo: String?) {
+    public init(id: String?, description: String?, phone: String?, email: String?, web: String?, additionalInfo: String?) {
         self.id = id
         self.description = description
         self.phone = phone
         self.email = email
         self.additionalInfo = additionalInfo
+        self.web = web
     }
     
 }
